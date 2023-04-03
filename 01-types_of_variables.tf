ed39c607db1d6068b7b918d826708bf2e7025740
acenawaz01 <46965770+acenawaz01@users.noreply.github.com>
2023-04-02 23:17:36 -0400

    Create 01-types_of_variables.tf

---
+++ 01-types_of_variables.tf
@@ -0,0 +1,19 @@
+variable "a-string" {
+    type = string
+}
+
+variable "this-is-a-number" {
+    type = number
+}
+
+variable "true-or-false" {
+    type = bool
+}
+
+#variable "a-string" {
+#    default = "this-is-a-string"
+#}
+
+variable "this-is-a-list" {
+   default = ["list of ", "Strings"]
+}
