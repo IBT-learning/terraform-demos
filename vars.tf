1a0a1f23cb892ae76cce25d88723f9178e5da2c4
Gunjan <gunjanvm7@gmail.com>
2023-06-16 18:36:35 -0400

    ec2 with provisioners

--- vars.tf
+++ vars.tf
@@ -1,11 +1,3 @@
-variable "AWS_ACCESS_KEY" {
-  default = "AKIAZFSH2TVGQPY7M56W"
-}
-
-variable "AWS_SECRET_KEY" {
-  default = "e9rSHLRQ1j8zp8HBgal0NyA1Cgq/ok+WZMIDVXZX"
-}
-
 variable "AWS_REGION" {
   default = "us-east-1"
 }
