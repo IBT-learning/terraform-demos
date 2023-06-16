773be2332c78afc095bd5707e9780ce034b273bb
Gunjan <gunjanvm7@gmail.com>
2023-06-16 18:22:53 -0400

    ec2_with_local_provisioner

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
   region     = var.AWS_REGION
 }
