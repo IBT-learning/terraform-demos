8f305fe027162acb3fb89de3cee6c393870cc807
Gunjan <gunjanvm7@gmail.com>
2023-06-19 18:32:52 -0400

    module

--- provider.tf
+++ provider.tf
@@ -5,18 +5,9 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "remote" {
-    organization = "ibt-learning"
-    workspaces {
-      name = "ibt-terraform-infra"
-    }
-  }
 }
-
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-west-2"
-  access_key = "<replace with your access key>"
-  secret_key = "<replace with your secret key>"
 }
 
